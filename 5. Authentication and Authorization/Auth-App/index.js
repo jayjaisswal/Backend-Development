fa0d184f1a23ab6bfc8fbc88e220327e69e0c500
jay kumar <kumarprince13833@gmail.com>
2025-01-13 11:01:14 +0530

    Directory name Updation

--- 5. Authentication and Authorization/Auth-App/index.js
+++ 5. Authentication and Authorization/Auth-App/index.js
@@ -4,6 +4,9 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
+const cookieParser = require("cookie-parser");
+aoo.use(cookieParser());
+
 app.use(express.json());
 
 require("./config/database").dbConnect();

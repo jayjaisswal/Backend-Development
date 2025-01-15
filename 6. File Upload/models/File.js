966ffb01d755b533a80864a4889967365ff00db7
jay kumar <kumarprince13833@gmail.com>
2025-01-15 22:51:18 +0530

    Emailing feature

--- 6. File Upload/models/File.js
+++ 6. File Upload/models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,37 @@ const fileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+// for email
+// post middleware
+fileSchema.post("save", async function (doc) {
+  // doc--> it refers to data entry of our DB
+  try {
+    console.log("DOC :", doc);
+
+    // transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: 587,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // this function must be written in config.js good practice
+    // send mail
+    let info = await transporter.sendMail({
+      from: "Jay the Great",
+      to: doc.email,
+      subject: "regarding placement",
+      html: `<h2> Hello sir/mam </h2> <p>Data Uploading</p> View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>`,
+    });
+    console.log("INFO :",info);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const File = mongoose.model("File", fileSchema);
